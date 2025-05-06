57002a3b990adfe7fe1bd29a717fa10c7424a5f2
Sdra Awameh <148454374+Sdra-Awameh@users.noreply.github.com>
2025-05-06 23:50:44 +0300

    AnimalTest.java

--- AnimalTest.java
+++ AnimalTest.java
@@ -2,98 +2,120 @@ class Animal {
     public int age;
     public String gender;
 
-    public Animal(int age, String gender) {
+    public Animal(int age, String gender)  
+    {
         this.age = age;
         this.gender = gender;
     }
 
-    public boolean isMammal() {
+    public boolean isMammal()
+    {
         return false;
     }
 
-    public void mate() {
+    public void mate() 
+    {
         System.out.println("Animal is mating.");
     }
 }
 
-class Duck extends Animal {
+class Duck extends Animal 
+{
     public String beakColor= "yellow";
 
-    public Duck(int age, String gender, String beakColor) {
+    public Duck(int age, String gender, String beakColor) 
+    {
         super(age, gender);
         this.beakColor = beakColor;
     }
 
-    public boolean isMammal() {
+    public boolean isMammal() 
+    {
         return false;
     }
 
-    public void mate() {
+    public void mate() 
+    {
         System.out.println("Duck is mating.");
     }
 
-    public void swim() {
+    public void swim() 
+    {
         System.out.println("Duck swims in the pond.");
     }
 
-    public void quack() {
+    public void quack() 
+    {
         System.out.println("Duck goes quack!");
     }
 }
 
-class Fish extends Animal {
+class Fish extends Animal 
+{
     private int sizeInFt;
     private boolean canEat;
 
-    public int getSizeInFt() {
+    public int getSizeInFt() 
+    {
         return sizeInFt;
     }
 
-    public boolean getCanEat() {
+    public boolean getCanEat() 
+    {
         return canEat;
     }
 
 
-    public Fish(int age, String gender, int sizeInFt, boolean canEat) {
+    public Fish(int age, String gender, int sizeInFt, boolean canEat)
+    {
         super(age, gender);
         this.sizeInFt = sizeInFt;
         this.canEat = canEat;
     }
 
-    public boolean isMammal() {
+    public boolean isMammal() 
+    {
         return false;
     }
 
-    public void mate() {
+    public void mate() 
+    {
         System.out.println("Fish is mating.");
     }
-    private void swim() {
+    private void swim() 
+    {
         System.out.println("Fish swims in the water.");
     }
 }
 
-class Zebra extends Animal {
+class Zebra extends Animal 
+{
     public boolean is_wild;
 
-    public Zebra(int age, String gender, boolean is_wild) {
+    public Zebra(int age, String gender, boolean is_wild) 
+    {
         super(age, gender);
         this.is_wild = is_wild;
     }
 
-    public boolean isMammal() {
+    public boolean isMammal() 
+    {
         return true;
     }
 
-    public void mate() {
+    public void mate()
+    {
         System.out.println("Zebra is mating.");
     }
 
-    public void run() {
+    public void run() 
+    {
         System.out.println("Zebra runs fast on the plains.");
     }
 }
 
-public class Test {
+public class Test 
+{
     public static void main(String[] args) {
         Duck duck = new Duck(2, "male", "red"); // test duck class
         System.out.println("Duck is a mammal: " + duck.isMammal());
